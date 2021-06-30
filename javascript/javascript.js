dfa7910ebef0ccda685ec020eb9da72c286b9d9e
Konstantin Nikolavih <KonstantinNikolavih@yandex.ru>
2021-06-30 20:15:58 +0300

    save_name

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,13 @@
 
-// 1 вареант
+// 1 вареант открытие popup
 
 const openPopup = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close');
 
+
+
+// переменная.команда(событие) функция перехвата команды
 openPopup.addEventListener('click', function() {
     popup.classList.add('popup_opened');
 })
@@ -14,6 +17,7 @@ closePopup.addEventListener('click', function() {
 })
 
 
+
 popup.addEventListener('click', function(event) {
   if (event.target === event.currentTarget) {
     popup.classList.toggle('popup_opened');
@@ -21,47 +25,64 @@ popup.addEventListener('click', function(event) {
 
 })
 
+// добовляет класс эфект плавного появления popup
+/* const popupm = document.querySelector('.popup_openedd');
+openPopup.addEventListener('mouseout', function() {
+  popupm.classList.toggle('popup_openedd'); */
+
+
+
 //тест формы для сохрпанения
 
 const formElement = document.querySelector('.popup__input');
 
 const nameInput = document.querySelector('.profile__name');
 const jobInput = document.querySelector('.profile__occupation');
+// сохранение редактированого профиля
 
 const save = document.querySelector('.popup__button-save');
 
+save.addEventListener('click', function() {
+  formElement.classList.remove('popup__button-save');
+})
+
+
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameInput.textContent = nameInput.Value;
-  jobInput.textContent = jobInput.Value;
-  save;
+  nameInput.textContent = nameInput.value;
+  jobInput.textContent = jobInput.value;
+  popup.classList.toggle('popup_opened');
+
 }
   formElement.addEventListener('submit', formSubmitHandler);
+
+
 
 //---
 
 
 // 2 вареант
 
-/* const openPopup = document.querySelector('.profile__button-add');
-const popup = document.querySelector('.popup');
-const closePopup = document.querySelector('.popup__close');
- */
+/* const openPopupAdd = document.querySelectorAdd('.profile__button-add');
+const popupAdd = document.querySelectorAdd('.popup');
+const closePopupAdd = document.querySelectorAdd('.popup__close');
 
-/* openPopup.addEventListener('click', function() {
-    popup.classList.add('nav_card');
+
+ openPopupAdd.addEventListener('click', function() {
+    popupAdd.classList.add('nav_card');
 })
 
-closePopup.addEventListener('click', function() {
-    popup.classList.remove('nav_card');
+closePopupAdd.addEventListener('click', function() {
+    popupAdd.classList.remove('nav_card');
 })
 
 
-popup.addEventListener('click', function(event) {
+popupAdd.addEventListener('click', function(event) {
   if (event.target === event.currentTarget) {
     popup.classList.toggle('nav_card');
   }
 
 })
-
  */
+
