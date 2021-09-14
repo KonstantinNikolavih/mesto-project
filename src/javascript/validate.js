e6ed75771461fa29c6f025c211ef1f3580372e6f
kostik <konstantinNikolavih@yandex.ru>
2021-09-14 23:48:22 +0300

    fix

--- src/javascript/validate.js
+++ src/javascript/validate.js
@@ -24,18 +24,35 @@ function setSubmit(form) {
 
 form.addEventListener('input', () => setSubmit(form)); */
 
-
-
-
-
 export function handleFormInput (event) {
   const input = event.target;
   const form = event.currentTarget;
 
   setCustomError(input);
   setFieldError(input)
-  setSubmitButtonState(form)
+  /* setSubmitButtonState(form) */
+  toggleButtonState(form)
 }
+
+
+/*Функция простой проверки на валидность хотя бы одного поля формы*/
+const hasInvalidInput = (inputs) => {
+  return inputs.some((input) => {
+    return !input.validity.valid
+  });
+}
+const toggleButtonState = (inputList, button, valid) => {
+  if (hasInvalidInput(inputList)) {
+    button.classList.add(valid.inActiveButtonClass);
+    button.disabled = true;
+  } else {
+      button.classList.remove(valid.inActiveButtonClass);
+      button.disabled = false;
+  }
+}
+
+
+
 
 // Найдём невалидные поля и установим ис тексты ошибок
 export function setCustomError(input) {
@@ -49,7 +66,7 @@ export function setFieldError(input) {
   span.textContent = input.validationMessage;
 }
 // Активируем или деактивируем кнопку
-export function setSubmitButtonState(form) {
+/* export function setSubmitButtonState(form) {
 
   const button = form.querySelector('.popup__button-save');
   const isValid = form.checkValidity();
@@ -62,7 +79,44 @@ if (isValid) {
   button.classList.add('popup__button-save_active');
   button.classList.remove('popup__button-save_disabled');
   button.setAttribute('disabled', 'disabled');
+  reset(button);
   }
-};
+}; */
+
+/* const setEventListeners = (formElement, valid) => {
+  const inputList = Array.from(formElement.querySelectorAll(valid.inputSelector));
+  const buttonSubmit = formElement.querySelector(valid.submitButtonSelector);
+  toggleButtonState(inputList, buttonSubmit, valid);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, valid);
+      toggleButtonState(inputList, buttonSubmit, valid);
+    })
+  })
+
+} */
 
 enableValidation(valid);
+
+//Сброс красной линии при невалидных данных
+/* function resetInputErr(popup) {
+  const inputList = Array.from(popup.querySelectorAll('.popup__item'));
+  inputList.forEach((input) => {
+      input.classList.remove('popup__item_error');
+  })
+} */
+
+//Сброс сообщения о невалидности значения ввода
+/* function resetSpanErr(popup) {
+  const errorElementList = popup.querySelectorAll('.popup__error');
+  errorElementList.forEach((errorElement)=> {
+      errorElement.classList.remove('popup__item_error');
+      errorElement.textContent = "";
+  })
+} */
+
+//Сброс ошибок валидации форм
+/* export function resetValidation(popup) {
+  resetSpanErr(popup)
+  resetInputErr(popup)
+} */
